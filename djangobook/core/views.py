1284b5d334675af20a3c289f716a234b072bce33
Eshan Pandey <pandeyeshan13@gmail.com>
2024-03-08 00:21:48 +0530

    signup page

--- djangobook/core/views.py
+++ djangobook/core/views.py
@@ -3,3 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def signup(request):
+
+    if(request.method == 'POST'):
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        print(username,email,password)
+        return HttpResponse('User created')
+    else: 
+        return render(request,'signup.html')
