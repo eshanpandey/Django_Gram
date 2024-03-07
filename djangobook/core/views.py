0ccd461f16ebd4d60b5b0cef8a03abafc0e7fe26
Eshan Pandey <pandeyeshan13@gmail.com>
2024-03-08 01:02:50 +0530

    finished up with signup and creating profiles for new users

--- djangobook/core/views.py
+++ djangobook/core/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import Profile
+
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
 def signup(request):
-
-    if(request.method == 'POST'):
+    if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
-        print(username,email,password)
-        return HttpResponse('User created')
-    else: 
-        return render(request,'signup.html')
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already taken')
+                return redirect('signup')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email already taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                messages.success(request, 'User created successfully')
+                #log user in and send them to settings page
+                #create profile for user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
