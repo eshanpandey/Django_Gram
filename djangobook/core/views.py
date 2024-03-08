9bda82fdcd0d4d545dbcd747980b6a845551c547
Eshan Pandey <pandeyeshan13@gmail.com>
2024-03-08 17:21:51 +0530

    logout functionality

--- djangobook/core/views.py
+++ djangobook/core/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .models import Profile
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='/signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -36,22 +40,27 @@ def signup(request):
     else:
         return render(request, 'signup.html')
 
-
 def signin(request):
+    
+    
     if request.method == 'POST':
         username = request.POST['username']
         password= request.POST['password']
-        user= User.objects.filter(username=username).first()
+        user= auth.authenticate(username=username, password=password)
 
         if user is None:
-            messages.error(request, 'Username does not exist')
+            messages.error(request, 'No such user exists. Please sign up.')
             return redirect('signin')
         else:
             if user.check_password(password):
-                return redirect('signin')
+                return redirect('/')
             else:
                 messages.error(request, 'Incorrect username or password')
                 return redirect('signin')
             
     else:
         return render(request, 'signin.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
